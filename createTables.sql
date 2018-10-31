7eebb56c84043ee4975237943d8bd7ab8a06689f
studente <studente@liceospalla.local>
2018-10-31 10:39:16 +0100

    create tables script

--- createTables.sql
+++ createTables.sql
@@ -1,40 +1,57 @@
-CREATE TABLE Content(
-  id int(11),
-  title varchar(255),
-  PermaLink varchar(255),
-  PRIMARY KEY (id), AUTO_INCREMENT
+CREATE TABLE Contents(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255),
+  permalink varchar(255),
+  description text,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Categories(
-  id int(11),
+  id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(255),
-  permalink varchar (255),
-  PRIMARY KEY (id), AUTO_INCREMENT
+  permalink varchar(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Tags(
-  id int(11),
+  id int(11) NOT NULL AUTO_INCREMENT,
   name varchar(255),
-  permalink varchar (255),
-  PRIMARY KEY (id), AUTO_INCREMENT
+  color varchar(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE Attachments(
-  id int(11),
-  permalink varchar (255),
+  id int(11) NOT NULL AUTO_INCREMENT,
+  href varchar(255),
   contentType varchar(255),
   description varchar(255),
-  PRIMARY KEY (id), AUTO_INCREMENT
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE ContentsCategoriesTh(
-
+  id int(11) NOT_NULL AUTO_INCREMENT,
+  contentId int(11),
+  categoryId int(11),
+  PRIMARY KEY (id),
+  FOREIGN KEY (contentId) REFERENCES Contents(id),
+  FOREIGN KEY (categoryId) REFERENCES Categories(id)
 );
 
 CREATE TABLE ContentsTagsTh(
+  id int(11) NOT_NULL AUTO_INCREMENT,
+  contentId int(11),
+  tagId int(11),
+  PRIMARY KEY (id),
+  FOREIGN KEY (contentId) REFERENCES Contents(id),
+  FOREIGN KEY (tagId) REFERENCES Tags(id)
 
 );
 
 CREATE TABLE ContentsAttachmentsTh(
-
+  id int(11) NOT_NULL AUTO_INCREMENT,
+  contentId int(11),
+  attachmentId int(11),
+  PRIMARY KEY (id),
+  FOREIGN KEY (contentId) REFERENCES Contents(id),
+  FOREIGN KEY (attachmentId) REFERENCES Attachments(id)
 );
