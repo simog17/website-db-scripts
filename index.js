47cf04829315269f0c87059f3a7d60c8877a27cd
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-11-14 10:43:10 +0100

    aggiunto caricamento degli scirpt di creazione delle tables

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const mysql = require('mysql');
+const config = require("./db-config-local.json")
+const mysql = require("mysql")
+const { readFileSync } = require("fs")
+const { join } = require("path")
+
+const sql = readFileSync(join(__dirname, "createTables.sql"), "utf8")
+
 const pool = mysql.createPool({
   connectionLimit : 10,
-  //todo
+  multipleStatements: true,
+  ...config
 });
+
+pool.query(sql, (err, results) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(results)
+  }
+})
